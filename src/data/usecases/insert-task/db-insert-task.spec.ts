517ad086efab0657ec4c628bfedf53b7eb83cc81
Daniel G Guerra <danielgguerra2@gmail.com>
2023-10-04 07:09:21 -0300

    test: ensure throws if InsertTaskRepository throws

--- src/data/usecases/insert-task/db-insert-task.spec.ts
+++ src/data/usecases/insert-task/db-insert-task.spec.ts
@@ -3,37 +3,52 @@ import { InsertTaskModel } from "../../../domain/usecases/insert-task.usecase"
 import { InsertTaskRepository } from "../protocols/insert-task-repository"
 import { DbInsertTask } from "./db-insert-task"
 
+const makeFakeTaskData = (): InsertTaskModel => ({
+  title: 'valid_title',
+  description: 'valid_description',
+  date: new Date(2023, 1, 1, 1, 1)
+})
+
+const makeFakeTaskModel = (): TaskModel => ({
+  id: 'valid_id',
+  title: 'valid_title',
+  description: 'valid_description',
+  date: new Date(2023, 1, 1, 1)
+})
+
+const makeInsertTaskRepositoryStub = (): InsertTaskRepository => {
+  class InsertTaskRepositoryStup implements InsertTaskRepository {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    insert (taskData: InsertTaskModel): Promise<TaskModel> {
+      return Promise.resolve(makeFakeTaskModel())
+    }
+  }
+  return new InsertTaskRepositoryStup()
+}
+
+const makeSut = () => {
+  const insertTaskRepositoryStup = makeInsertTaskRepositoryStub()
+  const sut = new DbInsertTask(insertTaskRepositoryStup)
+  return {
+    sut,
+    insertTaskRepositoryStup
+  }
+}
+
 describe('DbInsertTask Usecase', () => {
   test('should call InsertTaskRepository with correct values', async () => {
-    class InsertTaskRepositoryStup implements InsertTaskRepository {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      insert (taskData: InsertTaskModel): Promise<TaskModel> {
-        return Promise.resolve({
-          id: 'valid_id',
-          title: 'valid_title',
-          description: 'valid_description',
-          date: new Date(2023, 1, 1, 1)
-        })
-      }
-    }
-    
-    const insertTaskRepositoryStup = new InsertTaskRepositoryStup()
-    const sut = new DbInsertTask(insertTaskRepositoryStup)
-
+    const { sut, insertTaskRepositoryStup } = makeSut()
     const insertSpy = jest.spyOn(insertTaskRepositoryStup, 'insert')
+    await sut.insert(makeFakeTaskData())
+    expect(insertSpy).toHaveBeenCalledWith(makeFakeTaskData())
+  })
+  test('should throws if InsertTaskRepository throws', async () => {
+    const { sut, insertTaskRepositoryStup } = makeSut()
+    
+    jest.spyOn(insertTaskRepositoryStup, 'insert').mockResolvedValueOnce(Promise.reject(new Error()))
 
-    const taskModel = {
-      title: 'valid_title',
-      description: 'valid_description',
-      date: new Date(2023, 1, 1, 1, 1)
-    }
+    const response = sut.insert(makeFakeTaskData())
 
-    await sut.insert(taskModel)
-
-    expect(insertSpy).toHaveBeenCalledWith({
-      title: 'valid_title',
-      description: 'valid_description',
-      date: new Date(2023, 1, 1, 1, 1)
-    })
+    expect(response).rejects.toThrow()
   })
 })
